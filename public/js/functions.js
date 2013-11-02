5cab7ed626e9cb5dfa1aebd4cc02455e6d2102d6
David Grilli <djgrill@gmail.com>
2013-11-02 13:27:50 +0000

    Improves code documentation and conventions

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,101 +1,129 @@
-$(function() {
-  // CONSTANTS
-  var DECIMAL_MULTIPLIER = 100;
-  var VALID_COINS = [2, 1, .5, .2, .02, .01];
+// CONSTANTS
+var DECIMAL_MULTIPLIER = 100;
+var VALID_COINS = [2, 1, 0.5, 0.2, 0.02, 0.01];
 
-  /**
-  * SterlingValidator constructor
-  **/
-  function SterlingValidator(input) {
-    this.input = input;
-    this.pattern = /^\xA3?\d+(\.\d+p?)?$|^\d+p?$/;
 
-    this.validate = function() {
-      return this.pattern.test(this.input);
-    };
+/**
+ * Used to validate an input that represents an Sterling value
+ * @constructor
+ * @param {string} input Input text from the user
+ */
+function SterlingValidator(input) {
+  this.input = input;
+  this.pattern = /^\xA3?\d+(\.\d+p?)?$|^\d+p?$/;
+
+  // returns true if this.input matches this.pattern
+  this.validate = function() {
+    return this.pattern.test(this.input);
   };
+}
 
-  /**
-  * PenniesCalculator constructor
-  **/
-  function PenniesCalculator(numeric_input) {
-    this.numeric_input = numeric_input;
-  };
 
-  PenniesCalculator.prototype.calculate = function() {
-    // in this object we'll register the amount of each type of coin needed
-    var coins = {};
+/**
+ * Calculates the minimum amount of VALID_COINS needed to make the specified amount
+ * @constructor
+ * @param {float} numericInput Valid amount specified by the user
+ */
+function PenniesCalculator(numericInput) {
+  this.numericInput = numericInput;
+}
 
-    function updateResultCoins(coin) {
-      if (!coins[coin]) {
-        coins[coin] = 1;
-      }
-      else {
-        coins[coin] += 1;
-      }
-    };
+PenniesCalculator.prototype.calculate = function() {
+  // here we'll store the amount of each type of coin needed
+  var resultCoins = {};
 
-    _.each(VALID_COINS, function(coin, index) {
-      while (this.numeric_input >= coin) {
-        updateResultCoins(coin);
-        this.numeric_input -= coin;
-
-        // make sure 'numeric_input' is properly rounded,
-        // -- operations with floats are not always 100% precise
-        this.numeric_input = roundToDecimalPlace(this.numeric_input);
-      }
-    }, this);
-
-    return coins;
-  };
-
-  /**
-  * Helper functions
-  **/
-  function convertToValidFloat(str) {
-    // parseFloat() doesn't work properly with the unicode '£' character,
-    // -- so first we get the "numeric" part of the string with a regex
-    var number = /\d+(\.\d+)?/.exec(str)[0];
-
-    // check if the input refers to 'pences' only. In that case, convert to
-    // -- the POUND.pence version with decimal numbers (determined by DECIMAL_MULTIPLIER)
-    if (/\./.test(number) == false) {
-      number /= DECIMAL_MULTIPLIER;
-    }
-
-    return parseFloat(number);
-  };
-
-  function roundToDecimalPlace(number) {
-    return Math.round(number * DECIMAL_MULTIPLIER) / DECIMAL_MULTIPLIER;
-  };
-
-  function validateAndCalculate(input) {
-    var validator = new SterlingValidator(input);
-    if (validator.validate()) {
-      var numeric_input = roundToDecimalPlace(convertToValidFloat(input));
-      var calculator = new PenniesCalculator(numeric_input);
-      var result = calculator.calculate();
-      printResult(result);
+  function updateResultCoins(coin) {
+    if (!resultCoins[coin]) {
+      resultCoins[coin] = 1;
     }
     else {
-      // show error message
+      resultCoins[coin] += 1;
     }
-  };
+  }
 
-  function printResult(result) {
-    var result_keys = Object.keys(result).sort().reverse();
-    var $ul = $("<ul>");
+  _.each(VALID_COINS, function(coin, index) {
+    while (this.numericInput >= coin) {
+      updateResultCoins(coin);
+      this.numericInput -= coin;
 
-    _.each(result_keys, function(key) {
-      var $li = $("<li>");
-      $li.html(key + ": " + result[key]);
-      $ul.append($li);
-    });
+      // make sure that numericInput is properly rounded
+      this.numericInput = roundToDecimalPlace(this.numericInput);
+    }
+  }, this);
 
-    $("#result").html($ul);
-  };
+  return resultCoins;
+};
 
+
+/**
+ * Converts and returns a float number with 2 decimal values. For example:
+ * £1.25 => 1.25
+ * 125 => 1.25
+ * 25p => 0.25
+ * @param {string} str String to convert
+ */
+function convertToValidFloat(str) {
+  // parseFloat() doesn't work properly with the unicode '£' character,
+  // -- so first we get the "numeric" part of the string with a regex
+  var number = /\d+(\.\d+)?/.exec(str)[0];
+
+  // check if the input refers to 'pences' only. In that case, convert to
+  // -- the POUND.pence version with decimal numbers (determined by DECIMAL_MULTIPLIER)
+  if (/\./.test(number) === false) {
+    number /= DECIMAL_MULTIPLIER;
+  }
+
+  return parseFloat(number);
+}
+
+
+/**
+ * Returns the number rounded to de decimal place specified by DECIMAL_MULTIPLIER
+ * @param {number} number Number to convert
+ */
+function roundToDecimalPlace(number) {
+  return Math.round(number * DECIMAL_MULTIPLIER) / DECIMAL_MULTIPLIER;
+}
+
+
+/**
+ * Handles the validation of the user input
+ * If the input is valid, run the calculator. If not, show error message
+ * @param {string} input Input text form the user
+ */
+function validateAndCalculate(input) {
+  var validator = new SterlingValidator(input);
+  if (validator.validate()) {
+    var numericInput = roundToDecimalPlace(convertToValidFloat(input));
+    var calculator = new PenniesCalculator(numericInput);
+    var result = calculator.calculate();
+    showResult(result);
+  }
+  else {
+    $("#result").html("Error: wrong format");
+  }
+}
+
+
+/**
+ * Loads result in a friendly format into the page
+ * @param {object} result Data to show
+ */
+function showResult(result) {
+  // reverse the keys order to show result sorted by coin value
+  var resultKeys = Object.keys(result).sort().reverse();
+  var $ul = $("<ul>");
+
+  _.each(resultKeys, function(key) {
+    var $li = $("<li>");
+    $li.html(key + ": " + result[key]);
+    $ul.append($li);
+  });
+
+  $("#result").html($ul);
+}
+
+$(function() {
   $("#form-calculate").submit(function(e) {
     e.preventDefault();
     validateAndCalculate($("#input-amount").val());
